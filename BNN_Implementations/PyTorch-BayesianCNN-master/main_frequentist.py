ecc71bb6f2cd2e96c81743341eca840b0ff1db07
Nikhil Mehta <nikhilmehta.dce@gmail.com>
2020-03-31 18:29:26 -0400

    frequentist WIP

--- BNN_Implementations/PyTorch-BayesianCNN-master/main_frequentist.py
+++ BNN_Implementations/PyTorch-BayesianCNN-master/main_frequentist.py
@@ -13,7 +13,7 @@ import config_frequentist as cfg
 from models.NonBayesianModels.AlexNet import AlexNet
 from models.NonBayesianModels.LeNet import LeNet
 from models.NonBayesianModels.ThreeConvThreeFC import ThreeConvThreeFC
-
+import metrics
 # CUDA settings
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -44,22 +44,24 @@ def train_model(net, optimizer, criterion, train_loader):
 
 
 def validate_model(net, criterion, valid_loader):
-    valid_loss = 0.0
-    net.eval()
-    for data, target in valid_loader:
-        data, target = data.to(device), target.to(device)
-        output = net(data)
-        loss = criterion(output, target)
-        valid_loss += loss.item()*data.size(0)
-    return valid_loss
+	valid_loss = 0.0
+	accs = []
+	net.eval()
+	for data, target in valid_loader:
+		data, target = data.to(device), target.to(device)
+		output = net(data)
+		loss = criterion(output, target)
+		valid_loss += loss.item()*data.size(0)
+		accs.append(metrics.acc(output, target))
 
+	return valid_loss, accs
 
 def run(dataset, net_type):
 
     # Hyper Parameter settings
     n_epochs = cfg.n_epochs
     lr = cfg.lr
-    beta_type = cfg.beta_type
+    # beta_type = cfg.beta_type
     num_workers = cfg.num_workers
     valid_size = cfg.valid_size
     batch_size = cfg.batch_size
@@ -69,8 +71,8 @@ def run(dataset, net_type):
         trainset, testset, valid_size, batch_size, num_workers)
     net = getModel(net_type, inputs, outputs).to(device)
 
-    ckpt_dir = f'checkpoints/{dataset}/frequentist'
-    ckpt_name = f'checkpoints/{dataset}/frequentist/model_{net_type}.pt'
+    ckpt_dir = 'checkpoints/{}/frequentist'.format(dataset)
+    ckpt_name = 'checkpoints/{}/frequentist/model_{}.pt'.format(dataset, net_type)
 
     if not os.path.exists(ckpt_dir):
         os.makedirs(ckpt_dir, exist_ok=True)
@@ -95,11 +97,38 @@ def run(dataset, net_type):
             torch.save(net.state_dict(), ckpt_name)
             valid_loss_min = valid_loss
 
+def evaluate(dataset, net_type):
+
+	valid_size = cfg.valid_size
+	batch_size = cfg.batch_size
+	num_workers = cfg.num_workers
+
+	trainset, testset, inputs, outputs = data.getDataset(dataset)
+	train_loader, valid_loader, test_loader = data.getDataloader(
+		trainset, testset, valid_size, batch_size, num_workers)
+	net = getModel(net_type, inputs, outputs).to(device)
+
+	ckpt_dir = 'checkpoints/{}/frequentist'.format(dataset)
+	ckpt_name = 'checkpoints/{}/frequentist/model_{}.pt'.format(dataset, net_type)
+
+	net.load_state_dict(torch.load(ckpt_name))
+	criterion = nn.CrossEntropyLoss().to(device)
+	valid_loss, valid_acc = validate_model(net, criterion, test_loader)
+
+	valid_loss = valid_loss/len(test_loader.dataset)
+	valid_acc = np.mean(valid_acc)
+
+	print('Validation Loss: {:.4f} \tValidation Accuracy: {:.4f}'.format(valid_loss, valid_acc))	
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = "PyTorch Frequentist Model Training")
-    parser.add_argument('--net_type', default='alexnet', type=str, help='model')
-    parser.add_argument('--dataset', default='CIFAR10', type=str, help='dataset = [MNIST/CIFAR10/CIFAR100]')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description = "PyTorch Frequentist Model Training")
+	parser.add_argument('--net_type', default='lenet', type=str, help='model')
+	parser.add_argument('--dataset', default='MNIST', type=str, help='dataset = [MNIST/CIFAR10/CIFAR100]')
+	parser.add_argument('--evaluate', default='True', type=bool, help='')
+	args = parser.parse_args()
 
-    run(args.dataset, args.net_type)
+	if args.evaluate:
+		# evaluate the model here
+		evaluate(args.dataset, args.net_type)
+	else:
+		run(args.dataset, args.net_type)
